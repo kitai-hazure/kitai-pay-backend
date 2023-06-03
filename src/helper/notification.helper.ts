9985110bbe5c18f72254a54b59c5638e35350f25
kalashshah <202051096@iiitvadodara.ac.in>
2023-06-03 21:47:20 +0530

    :see_no_evil: notifications

--- src/helper/notification.helper.ts
+++ src/helper/notification.helper.ts
@@ -1,12 +1,16 @@
 import * as FCM from "fcm-node";
 import { ENV } from "../constants/env";
-import { ISendMessageType } from "../types/notification.types";
+import {
+  ISendMessageType,
+  ISendTopicMessageType,
+} from "../types/notification.types";
 
 const FCM_INSTANCE = new FCM(ENV.FCM_SERVER_KEY);
 
 export const sendPushNotification = ({
   title,
   body,
+  data,
   fcmTokens,
 }: ISendMessageType) => {
   const MESSAGE_TO_SEND = {
@@ -15,10 +19,54 @@ export const sendPushNotification = ({
       title,
       body,
     },
-    // CAN INCLUDE DATA PROP TOO CHECK DOCS....
+    data,
   };
 
-  FCM_INSTANCE.send(MESSAGE_TO_SEND, (err, response) => {
+  sendMessage(MESSAGE_TO_SEND);
+};
+
+export const sendPushNotificationToTopic = ({
+  title,
+  body,
+  data,
+  topicName,
+}: ISendTopicMessageType) => {
+  const MESSAGE_TO_SEND = {
+    to: `/topics/${topicName}`,
+    notification: {
+      title,
+      body,
+    },
+    data,
+  };
+  sendMessage(MESSAGE_TO_SEND);
+};
+
+export const subscribeToTopic = (fcmTokens: string[], topicName: string) => {
+  FCM_INSTANCE.subscribeToTopic(fcmTokens, topicName, (err, response) => {
+    if (err) {
+      console.log("Error subscribing to topic: ", err);
+    } else {
+      console.log("Successfully subscribed to topic: ", response);
+    }
+  });
+};
+
+export const unsubscribeFromTopic = (
+  fcmTokens: string[],
+  topicName: string
+) => {
+  FCM_INSTANCE.unsubscribeToTopic(fcmTokens, topicName, (err, response) => {
+    if (err) {
+      console.log("Error unsubscribing from topic: ", err);
+    } else {
+      console.log("Successfully unsubscribed from topic: ", response);
+    }
+  });
+};
+
+const sendMessage = (message: any) => {
+  FCM_INSTANCE.send(message, (err, response) => {
     if (err) {
       console.log("Something has gone wrong!");
     } else {
--- src/types/notification.types.ts
+++ src/types/notification.types.ts
@@ -2,4 +2,12 @@ export interface ISendMessageType {
   title: string;
   body: string;
   fcmTokens: string[] | undefined;
+  data?: any;
+}
+
+export interface ISendTopicMessageType {
+  title: string;
+  body: string;
+  topicName: string;
+  data?: any;
 }
